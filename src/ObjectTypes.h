2863f3e5f02927633406b6ae63255dd5e337df3d
Moon519 <axel.paris69@gmail.com>
2015-04-25 11:37:13 +0100

    Added ObjectTypes.h file

---
+++ src/ObjectTypes.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_TYPES
+#define OBJECT_TYPES
+
+// Define the possible actions return by a ReactToCollision
+
+typedef enum class ObjectTypes
+{
+    Other,
+    Player,
+    Enemy,
+    Missile,
+} ObjectTypes;
+
+#endif
